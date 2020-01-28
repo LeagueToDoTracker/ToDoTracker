6c178c0abc6ce02c289d5080ddb7fefaf6f555dd
Syu125 <yusophia2011@gmail.com>
2020-01-28 12:20:00 -0800

    Write tasks to file
    Can only make one subtask

--- src/Screen.java
+++ src/Screen.java
@@ -1,3 +1,6 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
@@ -29,37 +32,46 @@ public class Screen {
             s = JOptionPane.showInputDialog("Which parent task?");
             task = JOptionPane.showInputDialog("what task");
         }
-        System.out.println("task: " + s);
+        //System.out.println("task: " + s);
         task t = new task();
         t.setMainTaskName(task);
         for (int j = 0; j < goal.getSubtasks().size(); j++) {
-        	System.out.println(goal.getTask(j).getMainTaskNam().toLowerCase());
+        	//System.out.println(goal.getTask(j).getMainTaskNam().toLowerCase());
             if(goal.getTask(j).getMainTaskNam().toLowerCase().equals(s.toLowerCase())){
-            	System.out.println("work");
                 goal.getTask(j).addTask(t);
-                System.out.println("Parent task: " + goal.getTask(j).getMainTaskNam());
-                System.out.println("Child task: " + task);
+                //System.out.println("Parent task: " + goal.getTask(j).getMainTaskNam());
+                //System.out.println("Child task: " + task);
             }
         }
         list();
     }
     void start() {
         goal.setMainTaskName(JOptionPane.showInputDialog("What's your task?"));
-        System.out.println(goal.getMainTaskNam());
+        //System.out.println(goal.getMainTaskNam());
     }
     void add(){
         task t = new task();
         t.setMainTaskName(JOptionPane.showInputDialog("What task do you want to add?"));
-        System.out.println(t.getMainTaskNam());
+        //System.out.println(t.getMainTaskNam());
         goal.addTask(t);
 
     }
     void list() {
-    	for (int i = 0; i < goal.subtasks.size(); i++) {
+    	try {
+    		FileWriter fw =  new FileWriter("src/job.txt");
+    		fw.write("Task: " + goal.getMainTaskNam() + "\n");
+    		for (int i = 0; i < goal.subtasks.size(); i++) {
 			System.out.println("Subtask: " + goal.getTask(i).getMainTaskNam());
+			fw.write("   " +goal.getTask(i).getMainTaskNam() + "\n"); 
 			for (int j = 0; j < goal.getTask(i).subtasks.size(); j++) {
 				System.out.println("Subtask of " + goal.getTask(i).getMainTaskNam()+ ": " + goal.getTask(i).getTask(j).getMainTaskNam());
+				fw.write("      " + goal.getTask(i).getTask(j).getMainTaskNam() + "\n");
 			}
+			fw.close();
 		}
+    	}catch(IOException e) {
+    		e.printStackTrace();
+    	}
+    	
     }
 }
